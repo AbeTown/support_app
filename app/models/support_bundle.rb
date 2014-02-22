b853d5d81479f987c2ebb96d3ba7fe6400c156f8
AbeTown <abe.stephenson@gmail.com>
2014-02-21 23:43:04 -0800

    Revert "Generated a model, think I want to scaffold instead, so I'm going to commit this and revert to an earlier commit."
    This reverts commit 994a56434b2018ad116dc7af7c8341fc1a4cbf28.

--- app/models/support_bundle.rb
+++
@@ -1,2 +0,0 @@
-class SupportBundle < ActiveRecord::Base
-end
--- db/migrate/20140222062347_create_support_bundles.rb
+++
@@ -1,8 +0,0 @@
-class CreateSupportBundles < ActiveRecord::Migration
-  def change
-    create_table :support_bundles do |t|
-
-      t.timestamps
-    end
-  end
-end
