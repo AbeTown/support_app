c9a80b81d9b2fe8618a5627044a206025d497791
AbeTown <abe.stephenson@gmail.com>
2014-02-24 22:02:05 -0800

    Added two saved scopes to the support_bundle model. Not sure how the variable will work out.

--- app/models/support_bundle.rb
+++ app/models/support_bundle.rb
@@ -1,3 +1,12 @@
 class SupportBundle < ActiveRecord::Base
 	belongs_to :company
+
+	def self.production_bundles
+		where(environment: 'Production' or 'production')
+
+
+	def self.revision revis
+		where(firmware_revision: revis)
+	end
+
 end
